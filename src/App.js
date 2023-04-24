4feb9f7de185c7c0921f323191b06caf3e846d75
dsen12 <115708991+dsen12@users.noreply.github.com>
2023-04-24 13:11:35 +0930

    added app border. weather component content.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-        <h1>React Weather App</h1>
         <Weather />
         <footer>
           This project was coded by D.Sen and is <a href='https://github.com/dsen12/react-weather-app' target='_blank' rel="noreferrer">open-sourced on GitHub</a>.
--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,35 @@ import './Weather.css';
 export default function Weather() {
     return (
         <div className="Weather" >
-            Hello
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                        <input type="search" className="form-control" placeholder="Enter a city.."/>
+                    </div>
+                    <div className="col-3">
+                        <input type="submit" className="btn btn-primary" value="Search"/>
+                    </div>
+                </div>
+            </form>
+            <h1>Tokyo</h1>
+            <div className="row">
+                <div className="col-9">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
+                    <p>15 ºC | F</p>
+                    <ul>
+                        <li>Precipitation: 40%</li>
+                        <li>Humidity: 26%</li>
+                        <li>Wind: 6 km/h</li>
+                    </ul>
+                </div>
+                <div className="col-3">
+                        <h4>Weather</h4>
+                    <ul>
+                        <li>Monday 12:00pm</li>
+                        <li>Cloudy</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
