5af9030bb8173a2ed0002feb4f3d7329c1d238b0
dsen12 <115708991+dsen12@users.noreply.github.com>
2023-04-24 12:46:28 +0930

    created weather component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Weather from './Weather';
 import './App.css';
 
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <div className='container'>
         <h1>React Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by D.Sen and is <a href='https://github.com/dsen12/react-weather-app' target='_blank' rel="noreferrer">open-sourced on GitHub</a>.
         </footer>
