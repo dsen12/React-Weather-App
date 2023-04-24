91bd0e10ff30952408f5a48725bbdbd312461ecb
dsen12 <115708991+dsen12@users.noreply.github.com>
2023-04-24 12:28:48 +0930

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-    <h1>React Weather App</h1>
-    <footer>
-      This project was coded by D.Sen and is <a href='https://github.com/dsen12/react-weather-app' target='_blank' rel="noreferrer">open-sourced on GitHub</a>.
-    </footer>
+      <div className='container'>
+        <h1>React Weather App</h1>
+        <footer>
+          This project was coded by D.Sen and is <a href='https://github.com/dsen12/react-weather-app' target='_blank' rel="noreferrer">open-sourced on GitHub</a>.
+        </footer>
+      </div>
     </div>
   );
 }
