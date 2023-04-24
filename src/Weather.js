be7c86dd7475fccefef2b234b8a33a73a89868be
dsen12 <115708991+dsen12@users.noreply.github.com>
2023-04-24 17:28:05 +0930

    added state management. added real time data.

--- src/Weather.js
+++ src/Weather.js
@@ -1,42 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import './Weather.css';
 
 export default function Weather() {
-    const apiKey = "c751fba46471dbd42df6fc43ae19a086";
-    let city = "Tokyo";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-    return (
-        <div className="Weather" >
-            <form>
+    const [ready, setReady] = useState(false);
+    const [weatherData, setWeatherData] = useState({});
+
+    function handleResponse(response) {
+        setReady(true);
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            wind: response.data.wind.speed, 
+            humidity: response.data.main.humidity,
+            description: response.data.weather[0].description,
+            icon: response.data.weather[0].icon,
+        });
+    }
+
+    function search() {
+        const apiKey = "2f3a84f610ac1012c680bf273e43949a";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+    
+
+    if (ready) {
+        return (
+            <div className="Weather" >
+                <form>
+                    <div className="row">
+                        <div className="col-9">
+                            <input type="search" className="form-control" autoFocus="on" placeholder="Enter a city.."/>
+                        </div>
+                        <div className="col-3">
+                            <input type="submit" className="btn btn-primary w-100" value="Search"/>
+                        </div>
+                    </div>
+                </form>
+                <h1>Tokyo</h1>
                 <div className="row">
-                    <div className="col-9">
-                        <input type="search" className="form-control" autoFocus="on" placeholder="Enter a city.."/>
+                    <div className="col-8 flex">
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
+                        <span className="temperature">{Math.round(weatherData.temperature)}</span><span className="units">ºC | F</span>
+                        <ul className="left" >
+                            <li>Precipitation: 40%</li>
+                            <li>Humidity: {weatherData.humidity} %</li>
+                            <li>Wind: {Math.round(weatherData.wind)} km/h</li>
+                        </ul>
                     </div>
-                    <div className="col-3">
-                        <input type="submit" className="btn btn-primary w-100" value="Search"/>
+                    <div className="col-4">
+                            <h4>Weather</h4>
+                        <ul className="right">
+                            <li>Monday 12:00pm</li>
+                            <li className="text-capitalize">{weatherData.description}</li>
+                        </ul>
                     </div>
-                </div>
-            </form>
-            <h1>Tokyo</h1>
-            <div className="row">
-                <div className="col-8 flex">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
-                    <span className="temperature">15</span><span className="units">ºC | F</span>
-                    <ul className="left" >
-                        <li>Precipitation: 40%</li>
-                        <li>Humidity: 26%</li>
-                        <li>Wind: 6 km/h</li>
-                    </ul>
-                </div>
-                <div className="col-4">
-                        <h4>Weather</h4>
-                    <ul className="right">
-                        <li>Monday 12:00pm</li>
-                        <li>Cloudy</li>
-                    </ul>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        search();
+        return "Loading...";
+    }
 }
