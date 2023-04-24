bc1e423b888c230de22e4e363fb3fe791e7aba6c
dsen12 <115708991+dsen12@users.noreply.github.com>
2023-04-24 14:23:30 +0930

    font & colour changes. updated css. added form.

--- src/Weather.js
+++ src/Weather.js
@@ -7,27 +7,27 @@ export default function Weather() {
             <form>
                 <div className="row">
                     <div className="col-9">
-                        <input type="search" className="form-control" placeholder="Enter a city.."/>
+                        <input type="search" className="form-control" autoFocus="on" placeholder="Enter a city.."/>
                     </div>
                     <div className="col-3">
-                        <input type="submit" className="btn btn-primary" value="Search"/>
+                        <input type="submit" className="btn btn-primary w-100" value="Search"/>
                     </div>
                 </div>
             </form>
             <h1>Tokyo</h1>
             <div className="row">
-                <div className="col-9">
+                <div className="col-8 flex">
                     <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
-                    <p>15 ºC | F</p>
-                    <ul>
+                    <span className="temperature">15</span><span className="units">ºC | F</span>
+                    <ul className="left" >
                         <li>Precipitation: 40%</li>
                         <li>Humidity: 26%</li>
                         <li>Wind: 6 km/h</li>
                     </ul>
                 </div>
-                <div className="col-3">
+                <div className="col-4">
                         <h4>Weather</h4>
-                    <ul>
+                    <ul className="right">
                         <li>Monday 12:00pm</li>
                         <li>Cloudy</li>
                     </ul>
