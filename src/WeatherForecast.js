8e5c713e91c3b5f7efca58a594212afde0be89c9
dsen12 <115708991+dsen12@users.noreply.github.com>
2023-04-27 23:34:16 +0930

    replaced api key. create loop to display 5 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -12,21 +12,28 @@ export default function WeatherForecast(props) {
     }
 
     if (loaded)  {
-    return (
-        <div className="WeatherForecast">
-            <div className="row">
-                <div className="col">
-                    <WeatherForecastDay data={forecastData[0]} />
+        return (
+            <div className="WeatherForecast">
+                <div className="row">
+                    {forecastData.map(function (dailyForecast, index) {
+                        if (index < 5) {
+                            return (
+                                <div className="col" key={index}>
+                                    <WeatherForecastDay data={dailyForecast} />
+                                </div>
+                            );
+                        }
+                    })}
                 </div>
             </div>
-        </div>
-    );
-
+         );
     } else {
-        let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+        let apiKey = "6bfa54f242cbb59343d4e58db578dc61";
         let longitude = props.coordinates.lon;
         let latitude = props.coordinates.lat;
         let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
+
+        return null;
     }
 }
